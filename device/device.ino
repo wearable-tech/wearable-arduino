aab17fd366f48f87788908c79f323da1131dc710
Álex Mesquita <alex.mesquita06@hotmail.com>
2015-10-30 10:49:17 -0200

    Adding Free Running Mode

--- device/device.ino
+++ device/device.ino
@@ -1,7 +1,22 @@
 const int analogInPin = A0;
 
+// Define various ADC prescaler
+const unsigned char PS_16 = (1 << ADPS2);
+const unsigned char PS_32 = (1 << ADPS2) | (1 << ADPS0);
+const unsigned char PS_64 = (1 << ADPS2) | (1 << ADPS1);
+const unsigned char PS_128 = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+
 void setup() {
     Serial.begin(9600);
+    pinMode(A0, INPUT);
+
+    // set up the ADC
+    ADCSRA &= ~PS_128;  // remove bits set by Arduino library
+
+    ADCSRA |= PS_64;    // set our own prescaler to 64
+    // ADCSRA |= PS_32;    // set our own prescaler to 32
+    // ADCSRA |= PS_16;    // set our own prescaler to 16
 }
 
 void loop() {
