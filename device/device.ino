0a8813bf7cacca8935c193c7fca7b11214f7b581
Álex Mesquita <alex.mesquita06@hotmail.com>
2015-12-08 11:51:31 -0200

    Calculate oximetry and frequency

--- device/device.ino
+++ device/device.ino
@@ -1,128 +1,170 @@
 #include <math.h>
 
-#define ANALOG_RED A0
-#define ANALOG_I_RED A1
-#define MAX 200
+#define MAX 250
+#define PEAK 450
+#define INFRARED 7
+#define RED 8
 
-unsigned long red_values[100];
-unsigned long i_red_values[100];
+int i = 0;
+int time_readed = 0;
+int max1 = 0, max2 = 0, max3 = 0, max4 = 0;
+int max1_time = 0, max2_time = 0, max3_time = 0, max4_time = 0;
+int min1 = 0, min2 = 0, min3 = 0, min4 = 0;
+bool going_up = false;
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(13, OUTPUT);
-    delay(2000);
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  pinMode(INFRARED, OUTPUT);
+  pinMode(RED, OUTPUT);
+  Serial.println("CLEARDATA");
+  Serial.println("LABEL,Time,valor_vermelho,tempo_vermelho,valor_infra, tempo_infra");
+}
+
+void define_maximums(int previous, int value_readed) {
+  if (value_readed > PEAK) {
+    if ((previous > value_readed) && going_up) {
+      if (max1 == 0) {
+        max1_time = time_readed;
+        max1 = previous;
+      }
+      else if (max2 == 0) {
+        max2 = previous;
+        max2_time = time_readed;
+      }
+      else if (max3 == 0) {
+        max3 = previous;
+        max3_time = time_readed;
+      }
+      else if (max4 == 0) {
+        max4 = previous;
+        max4_time = time_readed;
+      }
+    }
+  }
+}
+
+void define_minimums(int previous, int value_readed) {
+  if ((previous < value_readed) && !going_up) {
+    if (min1 == 0) {
+      min1 = previous;
+    }
+    else if (min2 == 0) {
+      min2 = previous;
+    }
+    else if (min3 == 0) {
+      min3 = previous;
+    }
+    else if (min4 == 0) {
+      min4 = previous;
+    }
+  }
+}
+
+double calculate_frequency() {
+  Serial.print("DATA,TIME,"); Serial.print("max1"); Serial.print(","); Serial.print(max1_time); Serial.print("\n");
+  Serial.print("DATA,TIME,"); Serial.print("max2"); Serial.print(","); Serial.print(max2_time); Serial.print("\n");
+  Serial.print("DATA,TIME,"); Serial.print("max3"); Serial.print(","); Serial.print(max3_time); Serial.print("\n");
+  Serial.print("DATA,TIME,"); Serial.print("max4"); Serial.print(","); Serial.print(max4_time); Serial.print("\n");
+  double frequency1 = 1 / ((max2_time - max1_time) / 1000.0);
+  double frequency2 = 1 / ((max3_time - max2_time) / 1000.0);
+  double frequency3 = 1 / ((max4_time - max3_time) / 1000.0);
+  
+  int count = 0;
+  double sum = 0;
+  
+  if(frequency1 < 3.3333 && frequency1 > 0.5) {
+    count++;
+    sum += frequency1;
+  }
+  if(frequency2 < 3.3333 && frequency2 > 0.5) {
+    count++;
+    sum += frequency2;
+  }
+  if(frequency3 < 3.3333 && frequency3 > 0.5) {
+    count++;
+    sum += frequency3;
+  }
+
+  double frequency = 0;
+  if (count)
+    frequency = sum / count;
+  
+  Serial.print("DATA,TIME,"); Serial.print("frequencia1"); Serial.print(","); Serial.print(frequency1); Serial.print("\n");
+  Serial.print("DATA,TIME,"); Serial.print("frequencia2"); Serial.print(","); Serial.print(frequency2); Serial.print("\n");
+  Serial.print("DATA,TIME,"); Serial.print("frequencia3"); Serial.print(","); Serial.print(frequency3); Serial.print("\n");
+  Serial.print("DATA,TIME,"); Serial.print("frequencyuencia"); Serial.print(","); Serial.print(frequency); Serial.print("\n");
+  Serial.print("DATA,TIME,"); Serial.print("BPM"); Serial.print(","); Serial.print(frequency*60); Serial.print("\n");
+
+  return frequency;
+}
+
+void define_points(int led) {
+  max1 = 0;
+  max2 = 0;
+  max3 = 0;
+  max4 = 0;
+  min1 = 0;
+  min2 = 0;
+  min3 = 0;
+  min4 = 0;
+  int previous = 0;
+  
+  digitalWrite(led, HIGH);
+  delay(2000);
+  for(int i = 0; i < MAX; i++) {
+    time_readed = millis();
+  
+    int value_readed = analogRead(A0);
+  
+    define_maximums(previous, value_readed);
+    define_minimums(previous, value_readed);
+    
+    if (previous < value_readed) {
+      going_up = true;
+    }
+    else if (previous > value_readed) {
+      going_up = false;
+    }
+
+    previous = value_readed;
+    
+    if(led == RED) {
+      Serial.print("DATA,TIME,"); Serial.print(value_readed); Serial.print(","); Serial.print(time_readed); Serial.print(",,"); Serial.print("\n"); 
+    }
+    else {
+      Serial.print("DATA,TIME,"); Serial.print(",,"); Serial.print(value_readed); Serial.print(","); Serial.print(time_readed); Serial.print("\n"); 
+    }
+  }
+  digitalWrite(led, LOW);
+}
+
+double calculate_oximetry(double average_red_valleys, double average_red_peaks, double average_infrared_valleys, double average_infrared_peaks) {
+  double R = log(average_red_valleys / average_red_peaks) / log(average_infrared_valleys / average_infrared_peaks);
+  double oximetry = 110 - 25 * R;
+
+  Serial.print("DATA,TIME,"); Serial.print(",,"); Serial.print("R"); Serial.print(","); Serial.print(R); Serial.print("\n");
+  Serial.print("DATA,TIME,"); Serial.print(",,"); Serial.print("Oxigenacao"); Serial.print(","); Serial.print(oximetry); Serial.print("\n");
+
+  return oximetry;
 }
 
 void loop() {
-    /* int oximetry = calculate_oximetry();
-    int pulsation = calculate_pulsation();
-    Serial.println(oximetry * 1.0);
-    Serial.println(pulsation * 1.0);
-    // calculate_ratio();
-    delay(5000);
-    */
-    int buffer_red[MAX];
-    int buffer_i_red[MAX];
-    
-    // ligar vermelhor
-    digitalWrite(13, HIGH);
-    for(int i = 0; i < MAX; i++)
-    {
-       buffer_red[i] = analogRead(ANALOG_RED);
-    }
-    digitalWrite(13, LOW);
-    
-    delay(2000);
-    digitalWrite(13, HIGH);
-    
-    for(int i = 0; i < 200; i++)
-    {
-       buffer_i_red[i] = analogRead(ANALOG_I_RED);
-    }
+   define_points(RED);
+   
+   double frequency = calculate_frequency();
+   
+   double average_red_peaks = (max1 + max2 + max3 + max4) / 4.0;
+   double average_red_valleys = (min1 + min2 + min3 + min4) / 4.0;
+   Serial.print("DATA,TIME,"); Serial.print("media_average_red_peaks"); Serial.print(","); Serial.print(average_red_peaks); Serial.print("\n");
+   Serial.print("DATA,TIME,"); Serial.print("media_average_red_valleys"); Serial.print(","); Serial.print(average_red_valleys); Serial.print("\n");
+   
+   define_points(INFRARED);
+   double average_infrared_peaks = (max1 + max2 + max3 + max4) / 4.0;
+   double average_infrared_valleys = (min1 + min2 + min3 + min4) / 4.0;
+   Serial.print("DATA,TIME,"); Serial.print(",,"); Serial.print("media_average_infrared_peaks"); Serial.print(","); Serial.print(average_infrared_peaks); Serial.print("\n");
+   Serial.print("DATA,TIME,"); Serial.print(",,"); Serial.print("media_average_infrared_valleys"); Serial.print(","); Serial.print(average_infrared_valleys); Serial.print("\n");
 
-    digitalWrite(13, LOW);
-    
-    for(int i = 0; i < 200; i++)
-    {
-      Serial.println(buffer_red[i]);
-      Serial.print("\n");
-    }
-    for(int i = 0; i < 200; i++)
-    {
-      Serial.println(buffer_i_red[i]);
-      Serial.print("\n");
-    }
-    delay(2000);
-}
+   double oximetry = calculate_oximetry(average_red_valleys, average_red_peaks, average_infrared_valleys, average_infrared_peaks);
 
-int calculate_oximetry() {
-    return random(0, 100);
-}
-
-int calculate_pulsation() {
-    int sensorValue = analogRead(ANALOG_RED);
-    return sensorValue;
-}
-
-void read_inputs() {
-    for (int i = 0; i < MAX; i++) {
-        red_values[i] = analogRead(ANALOG_RED);
-        i_red_values[i] = analogRead(ANALOG_I_RED);
-    }
-}
-
-double calculate_ratio() {
-    double red_max = 0, red_min = 1023, i_red_max = 0, i_red_min = 1023;
-
-    read_inputs();
-
-    for (int i = 0; i < MAX; i++) {
-        if (red_max < red_values[i]) {
-            red_max = red_values[i];
-        }
-        if (red_min > red_values[i]) {
-            red_min = red_values[i];
-        }
-
-        if (i_red_max < i_red_values[i]) {
-            i_red_max = i_red_values[i];
-        }
-        if (i_red_min > i_red_values[i]) {
-            i_red_min = i_red_values[i];
-        }
-    }
-
-    for (int i = 0; i < MAX; i++) {
-      Serial.print(red_values[i]);
-      Serial.print(", ");
-    }
-    Serial.print("\n");
-    Serial.print("red_max:");
-    Serial.println(red_max);
-    Serial.print("red_min: ");
-    Serial.println(red_min);
-
-    for (int i = 0; i < MAX; i++) {
-      Serial.print(i_red_values[i]);
-      Serial.print(", ");
-    }
-    Serial.print("\n");
-    Serial.print("i_red_max:");
-    Serial.println(i_red_max);
-    Serial.print("i_red_min: ");
-    Serial.println(i_red_min);
-
-    double step = 5 / 1023.0;
-
-    red_max = red_max * step;
-    red_min = red_min * step;
-    i_red_max = i_red_max * step;
-    i_red_min = i_red_min * step;
-
-    double ratio = log(red_min / red_max) / log(i_red_min / i_red_max);
-
-    Serial.print("ratio: ");
-    Serial.println(ratio);
-
-    return ratio;
+   delay(300000);
 }
